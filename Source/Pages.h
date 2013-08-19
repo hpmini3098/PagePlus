12066442d775b8ff03e999ceb1a305437e5dcafb
Derek Chen <derek_chen@live.com>
2013-08-19 21:02:05 +0800

    commit shared data

--- Source/Pages.h
+++ Source/Pages.h
@@ -8,38 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum PullFlag : NSUInteger {
-	PullFlagNone = 0,
-	PullFlagLeft,
-	PullFlagRight
-} PullFlag;
+
 @protocol PagesDataSource;
 @protocol PagesDelegate;
 
-@interface AdditionView : UIView {
-	UILabel *_titleLabel;
-}
-@property (nonatomic, strong) UIImageView *imageView;
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, assign) BOOL isDone;
-@end
-
 @interface Pages : UIViewController <UIScrollViewDelegate> {
-	AdditionView *_leftView;
-	AdditionView *_rightView;
-	PullFlag _pullFlag;
-	NSMutableArray *_pageViews;
 	int _totalPageCount;
 }
 @property (nonatomic, assign) int currentIndex;
 @property (nonatomic, assign) id <PagesDataSource> dataSource;
 @property (nonatomic, assign) id <PagesDelegate> delegate;
 @property (nonatomic, strong) UIScrollView *container;
-@property (nonatomic, strong) UIView *currentView;
 
 - (id)initWithFrame:(CGRect)frame;
 - (void)reloadPages;
-- (UIView *)dequeueReusableViewWithIndex:(int)index;
+- (void)showThumbnail;
 @end
 @protocol PagesDataSource <NSObject>
 - (int)numberOfPages;
@@ -47,9 +30,7 @@ typedef enum PullFlag : NSUInteger {
 @end
 @protocol PagesDelegate <NSObject>
 
-- (void)pages:(Pages *)pages fromIndex:(int)fromIndex toIndex:(int)toIndex;
+- (void)pages:(Pages *)pages didSelected:(int)index;
 @optional
-- (void)pageWillChanged:(Pages *)pages;
-- (void)leftDidPullForPages:(Pages *)pages;
-- (void)rightDidPullForPages:(Pages *)pages;
+
 @end
--- Source/Pages.m
+++ Source/Pages.m
@@ -65,54 +65,13 @@
 
 @end
 
-#pragma mark - AdditionView
-#define IMAGE_SIZE CGSizeMake(20.0, 20.0)
-#define MIN_APLAHA 0.1
-#define MAX_APLAHA 1.0
-@implementation AdditionView
-
-- (id)initWithFrame:(CGRect)frame {
-	self = [super initWithFrame:frame];
-	if (self) {
-		self.backgroundColor = [UIColor clearColor];
-		self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, IMAGE_SIZE.width, IMAGE_SIZE.height)];
-        
-		self.imageView.center = CGPointMake(self.width / 2, self.height / 2);
-		self.imageView.alpha = MIN_APLAHA;
-		[self addSubview:self.imageView];
-		_titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.imageView.y + self.imageView.height + 8, self.width, 20)];
-		_titleLabel.textColor = [UIColor lightGrayColor];
-		_titleLabel.textAlignment = NSTextAlignmentCenter;
-		_titleLabel.font = [UIFont systemFontOfSize:14.0];
-		_titleLabel.adjustsFontSizeToFitWidth = YES;
-		_titleLabel.minimumScaleFactor = 0.6;
-		_titleLabel.backgroundColor = [UIColor clearColor];
-		[self addSubview:_titleLabel];
-	}
-	return self;
-}
-
-- (void)setTitle:(NSString *)title {
-	_title = title;
-	_titleLabel.text = title;
-}
-
-- (void)setIsDone:(BOOL)isDone {
-	_isDone = isDone;
-	[UIView animateWithDuration:0.3 animations: ^{
-	    self.imageView.alpha = isDone ? MAX_APLAHA : MIN_APLAHA;
-	}];
-}
-
-@end
 
 
 
 
 #pragma mark - Pages
-#define SLIDE_WIDTH 80.0
-#define SLIDE_DONE_X 60.0
-#define FRAME_OFFSET 30.0
+#define THUMBNAIL_SCALE 0.5
+#define THUMBNAIL_MARGIN_WITH 20.0
 @implementation Pages
 
 - (id)initWithFrame:(CGRect)frame {
@@ -122,34 +81,35 @@
 		self.container = [[UIScrollView alloc] initWithFrame:self.view.bounds];
 		self.container.delegate = self;
         
-		self.container.backgroundColor = [UIColor darkGrayColor];
+		self.container.backgroundColor = [UIColor clearColor];
 		self.container.showsVerticalScrollIndicator = NO;
 		self.container.showsHorizontalScrollIndicator = NO;
+        self.container.alwaysBounceVertical = NO;
 		self.container.pagingEnabled = YES;
 		self.container.alwaysBounceHorizontal = YES;
-		_leftView = [[AdditionView alloc] initWithFrame:CGRectMake(-SLIDE_WIDTH, 0, SLIDE_WIDTH, CGRectGetHeight(self.container.frame))];
-		[self.container addSubview:_leftView];
+        [self.view addSubview:self.container];
         
-		_rightView = [[AdditionView alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.container.frame), 0, SLIDE_WIDTH, CGRectGetHeight(self.container.frame))];
-		[self.container addSubview:_rightView];
-		[self.view addSubview:self.container];
+        self.currentIndex = 0;
 	}
 	return self;
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
-	[self reloadPages];
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
 }
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self reloadPages];
+}
+
 
 - (void)reloadPages {
 	if (!self.dataSource) {
 		return;
 	}
 	_totalPageCount = [self.dataSource numberOfPages];
-	if (!_pageViews) {
-		_pageViews = [[NSMutableArray alloc] initWithCapacity:_totalPageCount];
-	}
+	
     
 	self.container.contentSize = CGSizeMake(self.container.width * _totalPageCount, self.container.height);
 	[self insertPageAtIndex:self.currentIndex - 1];
@@ -158,132 +118,78 @@
 }
 
 - (void)insertPageAtIndex:(int)index {
-	if (!self.dataSource) {
-		return;
-	}
-	if (index < 0 || index >= _totalPageCount) {
-		return;
-	}
+    if (index<0 || index>=_totalPageCount || !self.dataSource) {
+        return;
+    }
+    CGRect rect = [self calculateNormalRectWithIndex:index];
+    UIView *view = [self.dataSource pages:self viewForindex:index];
+    view.frame = rect;
+    [self.container addSubview:view];
+}
+- (void)showThumbnail{
+    
+    float contentHeight = self.view.height * THUMBNAIL_SCALE;
+    float thumbnailWidth = self.view.width *THUMBNAIL_SCALE ;
+    float contentWidth = thumbnailWidth *(_totalPageCount+2) + THUMBNAIL_MARGIN_WITH *(_totalPageCount+1);
+    float y = (self.view.height - contentHeight)/2.5;
+    self.container.pagingEnabled = NO;
+    self.container.frame = CGRectMake(0, y, self.view.width, contentHeight);
+    self.container.contentSize = CGSizeMake(contentWidth, contentHeight);
     
     
-	UIView *view = [self.dataSource pages:self viewForindex:index];
-	if ([self.container.subviews containsObject:view]) {
-		return;
-	}
-	view.frame = [self bigFrameWithIndex:index];
-	[self.container addSubview:view];
-	[_pageViews insertObject:view atIndex:index];
+    for (int idx=0; idx<_totalPageCount; idx++) {
+        CGRect rect = [self getThumbnailRectWithIndex:idx];
+        UIView *view =[self.dataSource pages:self viewForindex:idx];
+        view.frame= rect;
+    }
+    [self scrollToViewWithIndex:self.currentIndex];
 }
 
-- (UIView *)dequeueReusableViewWithIndex:(int)index {
-	UIView *view = nil;
-	if (index >= 0 && index < [_pageViews count]) {
-		view = [_pageViews objectAtIndex:index];
-	}
-	return view;
-}
 
 #pragma mark - UIScrollViewDelegate Methods
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-	[UIView animateWithDuration:0.25 animations: ^{
-	    for (int i=0; i<[_pageViews count]; i++) {
-            UIView *v = [_pageViews objectAtIndex:i];
-            v.frame = [self smallFrameWithIndex:i];
-            v.layer.masksToBounds = NO;
-            v.layer.cornerRadius = 2; // if you like rounded corners
-            v.layer.shadowOffset = CGSizeMake(2, 2);
-            v.layer.shadowRadius = 2;
-            v.layer.shadowOpacity = 0.5;
-        }
-	} completion: ^(BOOL finished) {
-	}];
-    if (self.delegate && [self.delegate respondsToSelector:@selector(pageWillChanged:)]) {
-        [self.delegate pageWillChanged:self];
-    }
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-	float x = scrollView.contentOffset.x;
-	AdditionView *slideView = x < 0 ? _leftView : _rightView;
-	float absX = x > 0 ? x - scrollView.contentSize.width + scrollView.width : fabsf(x);
-	if (absX > SLIDE_DONE_X) {
-		slideView.isDone = YES;
-		_pullFlag = x > 0 ? PullFlagRight : PullFlagLeft;
-	}
-	else {
-		_pullFlag = PullFlagNone;
-	}
-}
 
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    float offsetX = scrollView.contentOffset.x;
+    self.currentIndex = [self calculateIndexWithOffset:offsetX];
+    [self scrollToViewWithIndex:self.currentIndex];
 	if (self.delegate) {
-		switch (_pullFlag) {
-			case PullFlagLeft:
-				if ([self.delegate respondsToSelector:@selector(leftDidPullForPages:)]) {
-					[self.delegate leftDidPullForPages:self];
-				}
-				break;
-                
-			case PullFlagRight:
-				if ([self.delegate respondsToSelector:@selector(rightDidPullForPages:)]) {
-					[self.delegate rightDidPullForPages:self];
-				}
-				break;
-                
-			default:
-				break;
-		}
+        [self.delegate pages:self didSelected:self.currentIndex];
 	}
+    
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-	CGFloat pageWidth = scrollView.frame.size.width;
-	int currentIndex = floor((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-	[self pageChanged:self.currentIndex toIndex:currentIndex];
-	self.currentIndex = currentIndex;
-	_pullFlag = PullFlagNone;
-	_leftView.isDone = NO;
-	_rightView.isDone = NO;
-    [UIView animateWithDuration:0.25 animations: ^{
-	    for (int i=0; i<[_pageViews count]; i++) {
-            UIView *v = [_pageViews objectAtIndex:i];
-            v.frame = [self bigFrameWithIndex:i];
-            v.layer.masksToBounds = YES;
-            v.layer.cornerRadius = 0; // if you like rounded corners
-            v.layer.shadowOffset = CGSizeMake(0, 0);
-            v.layer.shadowRadius = 0;
-            v.layer.shadowOpacity = 0.0;
-        }
-	} completion: ^(BOOL finished) {
-	}];
 }
 
 #pragma mark - Private Methods
-- (void)pageChanged:(int)fromIndex toIndex:(int)toIndex {
-	if (fromIndex < 0 || toIndex < 0 || fromIndex == toIndex) {
-		return;
-	}
-    if (self.delegate) {
-        [self.delegate pages:self fromIndex:fromIndex toIndex:toIndex];
-    }
-	if (self.dataSource) {
-		[self insertPageAtIndex:toIndex - 1];
-		[self insertPageAtIndex:toIndex];
-		[self insertPageAtIndex:toIndex + 1];
-	}
+- (CGRect)calculateNormalRectWithIndex:(int)index{
+    float x = index * self.view.width;
+    return CGRectMake(x, 0, self.view.width, self.view.height);
 }
 
-- (CGRect)smallFrameWithIndex:(int)index {
-	CGFloat x = self.container.width * index + FRAME_OFFSET;
-	CGFloat y = FRAME_OFFSET;
-	CGFloat width = self.container.width - FRAME_OFFSET * 2;
-	CGFloat height = self.container.height - FRAME_OFFSET * 2;
-	return CGRectMake(x, y, width, height);
+- (CGRect)getThumbnailRectWithIndex:(int)index{
+    float width = self.view.width * THUMBNAIL_SCALE;
+    float height = self.view.height *THUMBNAIL_SCALE;
+    float x = (index+1)*(width+THUMBNAIL_MARGIN_WITH);
+    return CGRectMake(x, 0, width, height);
 }
 
-- (CGRect)bigFrameWithIndex:(int)index {
-	CGFloat x = self.container.width * index;
-	return CGRectMake(x, 0.0, self.container.width, self.container.height);
+- (void)scrollToViewWithIndex:(int)index{
+    UIView *view = [self.dataSource pages:self viewForindex:index];
+    
+    float viewX = view.frame.origin.x;
+    float offsetX = viewX * (self.container.width/self.container.contentSize.width);
+    self.container.contentOffset = CGPointMake(offsetX, 0);
 }
+
+- (int)calculateIndexWithOffset:(int)offsetX{
+    float centerX = offsetX+self.container.width/2.0;
+    float width = self.container.width * THUMBNAIL_SCALE;
+    float indexf = centerX/(width+THUMBNAIL_MARGIN_WITH);
+    int index = index<self.currentIndex?ceil(indexf):floor(indexf);
+    
+    return index;
+}
+
 
 @end
